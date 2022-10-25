22db49193ab2c301323f3aa311a0b619ff93fc17
VnS Saber <26041218+VnSSaber@users.noreply.github.com>
2022-10-25 14:33:45 +0700

    Add files via upload

---
+++ Check Password Expired.ps1
@@ -0,0 +1,15 @@
+# Get our list of user names from the local file
+$users = Get-Content 'C:\Scripts\username.txt'
+
+# Create log file of output:
+$ReportDate = Get-Date -Format 'MM-dd-yyyy_hh-mm-ss'
+$ReportPath = "C:\Scripts\"
+$Report = $ReportPath + "PasswordExpiredReport_" + $ReportDate + ".txt"
+New-Item -path $Report -type File
+
+foreach ($user in $users) {
+"Checking user: $user" | out-file $Report -Append						#Notify user checked
+net user $user /domain | select-string 'Account active' >> $Report		#Obtain user information using net user, select only Account Active
+net user $user /domain | select-string 'Password expires' >> $Report 	#Obtain user information using net user, select only Password Expired
+Start-Sleep -Second 3 													#Pause to let system gather information
+}
